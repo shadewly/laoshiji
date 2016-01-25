5f98f961528c74909e6b49eec428d0be0a01183a
yuanxu2015 <zhu-yuan2015@outlook.com>
2016-01-25 10:34:41 -0800

    change the code structure

---
+++ codes/laoshifu/driveMaster/src/main/java/com/bz/service/impl/TestServiceImpl.java
@@ -0,0 +1,37 @@
+package com.bz.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bz.dao.TestDao;
+import com.bz.model.TestModel;
+import com.bz.service.TestServiceI;
+
+/**
+ * 目录服务类
+ * 
+ * @author yuxinchen
+ * 
+ */
+@Service(value = "testService")
+@Transactional
+public class TestServiceImpl implements TestServiceI {
+
+	@Autowired
+	@Qualifier("testDao")
+	protected TestDao testDao;
+
+	@Override
+	public List<TestModel> aa() throws Exception {
+		List<TestModel> list=testDao.selectTest();
+		System.out.println(list.size());
+		return list;
+	}
+
+
+	
+}
