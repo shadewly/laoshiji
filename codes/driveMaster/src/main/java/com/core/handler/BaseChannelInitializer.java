a17c35c62a5da06ead04bc851b979e712e55bed3
yuxinchen <yuxinchen@gitlab.com>
2016-01-27 17:20:02 +0800

    添加ssl

--- codes/driveMaster/src/main/java/com/core/handler/BaseChannelInitializer.java
+++ codes/driveMaster/src/main/java/com/core/handler/BaseChannelInitializer.java
@@ -6,15 +6,21 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+
+import java.io.File;
+
+import javax.servlet.ServletException;
+
+import junit.framework.Assert;
 
 import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.mock.web.MockServletConfig;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletException;
 
 /**
  *
@@ -46,10 +52,18 @@ public class BaseChannelInitializer extends ChannelInitializer<SocketChannel> {
 		// SecureChatSslContextFactory.getServerContext().createSSLEngine();
 		// engine.setUseClientMode(false);
 		// pipeline.addLast("ssl", new SslHandler(engine));
+		File certChainFile = new File("D:\\keys\\mycas.crt");
+		// Assert.notNull(certChainFile, "Certification file can't be null!");
+		File keyFile = new File("D:\\keys\\mycas.keystore");
+		// Assert.notNull(keyFile, "Key file can't be null!");
+		SslContext sslCtx = SslContext.newServerContext(certChainFile, keyFile);
+
 		pipeline.addLast("decoder", new HttpRequestDecoder());
 		pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
 		pipeline.addLast("encoder", new HttpResponseEncoder());
 		pipeline.addLast("chunkedWriter", new ChunkedWriteHandler());
+		pipeline.addLast("ssl",
+				new SslHandler(sslCtx.newEngine(channel.alloc())));
 		pipeline.addLast("ssoHandler", new SsoHandler(
 				this.delegatingFilterProxy));
 		pipeline.addLast("springMvcHandler", new ServletNettyHandler(
