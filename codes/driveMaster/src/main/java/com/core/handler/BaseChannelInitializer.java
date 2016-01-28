7bb7025235b57494fc50c6fc546deb8e03f156ce
wanglinyong <wanglinyong@shine-soft.com>
2016-01-28 20:24:16 +0800

    修改文件路径，未完成

--- codes/driveMaster/src/main/java/com/core/handler/BaseChannelInitializer.java
+++ codes/driveMaster/src/main/java/com/core/handler/BaseChannelInitializer.java
@@ -18,6 +18,7 @@ import junit.framework.Assert;
 
 import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.mock.web.MockServletConfig;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -27,48 +28,43 @@ import org.springframework.web.servlet.DispatcherServlet;
  */
 public class BaseChannelInitializer extends ChannelInitializer<SocketChannel> {
 
-	private final DispatcherServlet dispatcherServlet;
-	private final DelegatingFilterProxy delegatingFilterProxy;
+    private final DispatcherServlet dispatcherServlet;
+    private final DelegatingFilterProxy delegatingFilterProxy;
 
-	public BaseChannelInitializer(WebApplicationContext applicationContext)
-			throws ServletException {
-		// spring mvc
-		MockServletConfig servletConfig = new MockServletConfig();
-		this.dispatcherServlet = new DispatcherServlet(applicationContext);
-		this.dispatcherServlet.init(servletConfig);
+    public BaseChannelInitializer(WebApplicationContext applicationContext)
+            throws ServletException {
+        // spring mvc
+        MockServletConfig servletConfig = new MockServletConfig();
+        this.dispatcherServlet = new DispatcherServlet(applicationContext);
+        this.dispatcherServlet.init(servletConfig);
 
-		// sso
-		MockFilterConfig filterConfig = new MockFilterConfig();
-		this.delegatingFilterProxy = new DelegatingFilterProxy(
-				"authenticationFilter", applicationContext);
-		this.delegatingFilterProxy.init(filterConfig);
-	}
+        // sso
+        MockFilterConfig filterConfig = new MockFilterConfig();
+        this.delegatingFilterProxy = new DelegatingFilterProxy(
+                "authenticationFilter", applicationContext);
+        this.delegatingFilterProxy.init(filterConfig);
+    }
 
-	@Override
-	public void initChannel(SocketChannel channel) throws Exception {
-		ChannelPipeline pipeline = channel.pipeline();
-		// Uncomment the following line if you want HTTPS
-		// SSLEngine engine =
-		// SecureChatSslContextFactory.getServerContext().createSSLEngine();
-		// engine.setUseClientMode(false);
-		// pipeline.addLast("ssl", new SslHandler(engine));
-		File certChainFile = new File("D:\\keys\\mycas.crt");
-		// Assert.notNull(certChainFile, "Certification file can't be null!");
-		File keyFile = new File("D:\\keys\\mycas.keystore");
-		// Assert.notNull(keyFile, "Key file can't be null!");
-		SslContext sslCtx = SslContext.newServerContext(certChainFile, keyFile);
+    @Override
+    public void initChannel(SocketChannel channel) throws Exception {
+        ChannelPipeline pipeline = channel.pipeline();
+        // Uncomment the following line if you want HTTPS
+        File certChainFile = ResourceUtils.getFile("classpath:/resources/certification/cas_cert.pem");
+        com.common.util.Assert.notNull(certChainFile, "Certification file can't be null!");
+        File keyFile = ResourceUtils.getFile("classpath:/resources/certification/privateKey_pkcs8.pem");
+        com.common.util.Assert.notNull(keyFile, "Key file can't be null!");
+        SslContext sslCtx = SslContext.newServerContext(certChainFile, keyFile);
+        pipeline.addLast("ssl", new SslHandler(sslCtx.newEngine(channel.alloc())));
 
-		pipeline.addLast("decoder", new HttpRequestDecoder());
-		pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
-		pipeline.addLast("encoder", new HttpResponseEncoder());
-		pipeline.addLast("chunkedWriter", new ChunkedWriteHandler());
-		pipeline.addLast("ssl",
-				new SslHandler(sslCtx.newEngine(channel.alloc())));
-		pipeline.addLast("ssoHandler", new SsoHandler(
-				this.delegatingFilterProxy));
-		pipeline.addLast("springMvcHandler", new ServletNettyHandler(
-				this.dispatcherServlet));
+        pipeline.addLast("decoder", new HttpRequestDecoder());
+        pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
+        pipeline.addLast("encoder", new HttpResponseEncoder());
+        pipeline.addLast("chunkedWriter", new ChunkedWriteHandler());
+        pipeline.addLast("ssoHandler", new SsoHandler(
+                this.delegatingFilterProxy));
+        pipeline.addLast("springMvcHandler", new ServletNettyHandler(
+                this.dispatcherServlet));
 
-	}
+    }
 
 }
