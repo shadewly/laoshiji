f0b2d9b15c8f49064793cf98b5768fecfbb9903b
yuanxu2015 <zhu-yuan2015@outlook.com>
2016-02-13 16:08:48 +0800

    build file update

--- codes/laoshifu/client/src/main/java/com/lsf/login/RegisterActivity.java
+++ codes/laoshifu/client/src/main/java/com/lsf/login/RegisterActivity.java
@@ -3,8 +3,13 @@ package com.lsf.login;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -13,8 +18,38 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_register, menu);
+
+        MenuItem seachItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)MenuItemCompat.getActionView(seachItem);
+
+        //ToDo: Configure the search info and add any event listeners...
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                return true;
+            case R.id.action_favorite:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
