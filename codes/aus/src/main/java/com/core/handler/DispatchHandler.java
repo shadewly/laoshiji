728ce4072a802ed3253c6ef59765a1c1e3a768e5
yuxinchen <yuxinchen@gitlab.com>
2016-03-01 09:47:03 +0800

    修改整合netty返回

--- codes/aus/src/main/java/com/core/handler/DispatchHandler.java
+++ codes/aus/src/main/java/com/core/handler/DispatchHandler.java
@@ -3,15 +3,25 @@ package com.core.handler;
 import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
 import static io.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.springframework.mock.web.MockHttpServletResponse;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.stream.ChunkedStream;
 import io.netty.util.CharsetUtil;
 
 public class DispatchHandler extends
@@ -43,8 +53,21 @@ public class DispatchHandler extends
 	@Override
 	protected void messageReceived(ChannelHandlerContext channelHandlerContext,
 			FullHttpRequest fullHttpRequest) throws Exception {
-
+		MockHttpServletResponse servletResponse = new MockHttpServletResponse();
+		HttpResponseStatus status = HttpResponseStatus.valueOf(servletResponse
+				.getStatus());
+		HttpResponse response = new DefaultHttpResponse(HTTP_1_1, status);
 		System.out.println("555555555555555555555");
+		channelHandlerContext.write(response);
+
+		InputStream contentStream = new ByteArrayInputStream(
+				servletResponse.getContentAsByteArray());
+
+		// Write the content and flush it.
+		ChannelFuture writeFuture = channelHandlerContext
+				.writeAndFlush(new ChunkedStream(contentStream));
+		writeFuture.addListener(ChannelFutureListener.CLOSE);
+		fullHttpRequest.retain();
 
 	}
 
