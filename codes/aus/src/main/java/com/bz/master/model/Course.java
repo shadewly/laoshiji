368a08c5dcd02c0dcfefc4392380cefa7a1deae5
yuxinchen <yuxinchen@gitlab.com>
2016-03-02 17:15:45 +0800

    添加师傅model

---
+++ codes/aus/src/main/java/com/bz/master/model/Course.java
@@ -0,0 +1,34 @@
+/***********************************************************************
+ * Module:  Course.java
+ * Author:  Administrator
+ * Purpose: Defines the Class Course
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid 7035aec3-e9fc-4da3-8f69-ab5b3f913e96 */
+public class Course {
+   /** @pdOid 3c36aff2-2ccb-4323-a6ed-09efbaa75162 */
+   private Long id;
+   /** @pdOid 6d01bf0b-2c4e-47cc-8348-f3d1894eafbf */
+   private String courseNo;
+   /** @pdOid 0c7ac804-381a-44f5-8830-9cc463d9428c */
+   private String licenseType;
+   /** @pdOid bb0fc8db-2b60-4ef1-a970-c260492e1444 */
+   private String trainArea;
+   /** @pdOid 2535aeb6-079d-4303-b51d-c75c9009d045 */
+   private String trainStartTime;
+   /** @pdOid 2b2cbb82-5199-498d-9f7f-b1dd7b288fac */
+   private String trainEndTime;
+   /** @pdOid b2bcdca2-e99e-4b53-8de6-614c15f71eb1 */
+   private String trainType;
+   /** @pdOid e4599b7e-9231-4381-9605-68f41a4eda3c */
+   private String freeService;
+   /** @pdOid a65367fb-36d9-4019-bacb-3d4044133e52 */
+   private String comment;
+   /** @pdOid 542f6f0b-1332-4456-a46b-dd2094fbbc83 */
+   private Integer status;
+   /** @pdOid ed222f90-72e6-4090-aacc-71797905a311 */
+   private Integer apprenticeLimit;
+
+}
---
+++ codes/aus/src/main/java/com/bz/master/model/CourseSchedule.java
@@ -0,0 +1,40 @@
+/***********************************************************************
+ * Module:  CourseSchedule.java
+ * Author:  Administrator
+ * Purpose: Defines the Class CourseSchedule
+ ***********************************************************************/
+
+import java.util.*;
+
+/** @pdOid 2789b448-52c5-4571-a1d1-0f54421f2a88 */
+public class CourseSchedule {
+   /** @pdOid ff768de7-df4d-4ef9-8511-d79f9e414013 */
+   private Integer id;
+   /** @pdOid 9bcd6275-868f-4676-8bd6-d68ca8706a39 */
+   private String scheduleNo;
+   /** @pdOid 3b8d2696-fa38-4bd5-b2c0-5196eacb2fa7 */
+   private String courseNo;
+   /** @pdOid b97ac2ce-cf5c-4edd-8683-ab30b8e4b375 */
+   private String licenseType;
+   /** @pdOid a55b427c-9871-49b8-91b4-07979960a417 */
+   private String trainArea;
+   /** @pdOid 12dd6c3d-d962-493d-aada-848cab79332d */
+   private Date trainStartTime;
+   /** @pdOid 701e951d-96f7-4d02-9f04-f42b282d92e5 */
+   private Date trainEndTime;
+   /** @pdOid 01399cb9-fe59-48ba-b0c5-4df1dd0b8c9d */
+   private String trainType;
+   /** @pdOid b5f2a98d-38be-42c1-a41f-6b7c3ccb5c0a */
+   private String freeService;
+   /** @pdOid b3068e0c-bbd7-4419-896f-58811ae800b6 */
+   private String comment;
+   /** @pdOid a83815c9-01b1-4cf0-9453-bf4e31f3dd25 */
+   private Integer apprenticeLimit;
+   /** @pdOid cd4e7774-d9ff-480b-a132-ca9b2404b751 */
+   private String status;
+   /** @pdOid 47dff2ce-1d3d-4ad5-ba3a-e99e73df3a22 */
+   private Integer enrollment;
+   /** @pdOid 55ee8427-d6a0-46f7-981e-e665658e9530 */
+   private List erollmentList;
+
+}
