70fb7d0b2ee9052fd07d4be548feff6f9b1ed00f
yuxinchen <yuxinchen@gitlab.com>
2016-03-09 16:45:25 +0800

    更改为本地连接

--- codes/laoshifu/webServer/src/main/java/com/test/controller/Client.java
+++ codes/laoshifu/webServer/src/main/java/com/test/controller/Client.java
@@ -1,213 +1,172 @@
 package com.test.controller;
 
-import java.io.InputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpParams;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+
 
 public class Client {
-
-	// public static void main(String[] args) throws Exception {
-	// HttpClient client = new DefaultHttpClient();
-	// String path = "https://localhost:8443/cas/v1/tickets";
-	// HttpPost post = new HttpPost(path);
-	// // Vendor v = new Vendor();
-	// // v.setName("¥´ ‰ ˝æ›");
-	// // v.setDescription(" ˝æ›¥´ ‰");
-	// // v.setCreateDate(new Date());
-	// // v.setId(20);
-	// // String content = JSONBinder.binder(Vendor.class).toJSON(v);
-	// StringEntity entity = new StringEntity("username=yxc&password=123");
-	// entity.setContentEncoding("UTF-8");
-	// entity.setContentType("application/x-www-form-urlencoded");
-	// post.setEntity(entity);
-	// HttpResponse response = client.execute(post);
-	// System.out.println("œÏ”¶◊¥Ã¨¬Î£∫" + response.getStatusLine().getStatusCode());
-	// InputStream is = response.getEntity().getContent();
-	// // String text = StreamUtil.readInputStream(is);
-	// // System.out.println("∑˛ŒÒ∆˜∂ÀœÏ”¶µƒ ˝æ›£∫" + text);
-	// }
-
-	// ///////////////////////////////////////////////////////
-	// public static String getTicket(final String server, final String
-	// username,
-	// final String password, final String service) {
-	// notNull(server, "server must not be null");
-	// notNull(username, "username must not be null");
-	// notNull(password, "password must not be null");
-	// notNull(service, "service must not be null");
-	//
-	// return getServiceTicket(server,
-	// getTicketGrantingTicket(server, username, password), service);
-	// }
-	//
-	// /**
-	// * »°µ√ST
-	// *
-	// * @param server
-	// * @param ticketGrantingTicket
-	// * @param service
-	// */
-	// private static String getServiceTicket(final String server,
-	// final String ticketGrantingTicket, final String service) {
-	// if (ticketGrantingTicket == null)
-	// return null;
-	//
-	// final HttpClient client = new HttpClient();
-	//
-	// final PostMethod post = new PostMethod(server + "/"
-	// + ticketGrantingTicket);
-	//
-	// post.setRequestBody(new NameValuePair[] { new NameValuePair("service",
-	// service) });
-	//
-	// try {
-	// client.executeMethod(post);
-	//
-	// final String response = post.getResponseBodyAsString();
-	//
-	// switch (post.getStatusCode()) {
-	// case 200:
-	// return response;
-	//
-	// default:
-	// warning("Invalid response code (" + post.getStatusCode()
-	// + ") from CAS server!");
-	// info("Response (1k): "
-	// + response.substring(0,
-	// Math.min(1024, response.length())));
-	// break;
-	// }
-	// }
-	//
-	// catch (final IOException e) {
-	// warning(e.getMessage());
-	// }
-	//
-	// finally {
-	// post.releaseConnection();
-	// }
-	//
-	// return null;
-	// }
-
-	/**
-	 * @param server
-	 * @param username
-	 * @param password
-	 */
-	private static String getTicketGrantingTicket(final String server,
-			final String username, final String password) {
-		final HttpClient client = new DefaultHttpClient();
-		String server1 = "https://localhost:8443/cas/v1/tickets";
-		final HttpPost post = new HttpPost(server1);
-
-		try {
-			StringEntity entity = new StringEntity("username=yxc&password=123");
-			post.setEntity(entity);
-			;
-			final HttpResponse response = client.execute(post);
-
-			final int bodystr = response.getStatusLine().getStatusCode();
-			info("TGT=" + response);
-			// switch (post.getStatusCode()) {
-			// case 201: {
-			// final Matcher matcher = Pattern.compile(
-			// ".*action=\".*/(.*?)\".*").matcher(response);
-			//
-			// if (matcher.matches())
-			// return matcher.group(1);
-			//
-			// warning("Successful ticket granting request, but no ticket found!");
-			// info("Response (1k): "
-			// + response.substring(0,
-			// Math.min(1024, response.length())));
-			// break;
-			// }
-			//
-			// default:
-			// warning("Invalid response code (" + post.getStatusCode()
-			// + ") from CAS server!");
-			// info("Response (1k): "
-			// + response.substring(0,
-			// Math.min(1024, response.length())));
-			// break;
-			// }
-		}
-
-		catch (final Exception e) {
-			warning(e.getMessage());
-		}
-
-		finally {
-			post.releaseConnection();
-		}
-
-		return null;
-	}
-
-	// private static void ticketValidate(String serverValidate,
-	// String serviceTicket, String service) {
-	// notNull(serviceTicket, "paramter 'serviceTicket' is not null");
-	// notNull(service, "paramter 'service' is not null");
-	//
-	// final HttpClient client = new HttpClient();
-	// GetMethod post = null;
-	//
-	// try {
-	// post = new GetMethod(serverValidate + "?" + "ticket="
-	// + serviceTicket + "&service="
-	// + URLEncoder.encode(service, "UTF-8"));
-	// client.executeMethod(post);
-	//
-	// final String response = post.getResponseBodyAsString();
-	// info(response);
-	// switch (post.getStatusCode()) {
-	// case 200: {
-	// info("≥…π¶»°µ√”√ªß ˝æ›");
-	// }
-	// default: {
-	//
-	// }
-	// }
-	//
-	// } catch (Exception e) {
-	// warning(e.getMessage());
-	// } finally {
-	// //  Õ∑≈◊ ‘¥
-	// post.releaseConnection();
-	// }
-	//
-	// }
-
-	// private static void notNull(final Object object, final String message) {
-	// if (object == null)
-	// throw new IllegalArgumentException(message);
-	// }
-
-	public static void main(final String[] args) throws Exception {
-		final String server = "http://localhost:8080/CASServer/v1/tickets";
-		final String username = "username";
-		final String password = "username";
-		final String service = "http://localhost:8080/service"; // ÀÊ“‚–¥
-		// final String proxyValidate =
-		// "http://localhost:8080/<span style='font-family: Arial, Helvetica, sans-serif;'>CASServer</span>/proxyValidate";
-		//
-		// ticketValidate(proxyValidate,
-		// getTicket(server, username, password, service), service);
-
-		getTicketGrantingTicket(server, username, password);
-	}
-
-	private static void warning(String msg) {
-		System.out.println(msg);
-	}
-
-	private static void info(String msg) {
-		System.out.println(msg);
-	}
+	public static String getTicket(final String server, final String username, final String password,  
+            final String service) {  
+        notNull(server, "server must not be null");  
+        notNull(username, "username must not be null");  
+        notNull(password, "password must not be null");  
+        notNull(service, "service must not be null");  
+  
+        return getServiceTicket(server, getTicketGrantingTicket(server, username, password), service);  
+    }  
+  
+    /** 
+     * ÂèñÂæóST 
+     * @param server 
+     * @param ticketGrantingTicket 
+     * @param service 
+     */  
+    private static String getServiceTicket(final String server, final String ticketGrantingTicket, final String service) {  
+        if (ticketGrantingTicket == null)  
+            return null;  
+  
+        final HttpClient client = new HttpClient();  
+  
+        final PostMethod post = new PostMethod(server + "/" + ticketGrantingTicket);  
+  
+        post.setRequestBody(new NameValuePair[] { new NameValuePair("service", service) });  
+  
+        try {  
+            client.executeMethod(post);  
+  
+            final String response = post.getResponseBodyAsString();  
+  
+            switch (post.getStatusCode()) {  
+            case 200:  
+                return response;  
+  
+            default:  
+                warning("Invalid response code (" + post.getStatusCode() + ") from CAS server!");  
+                info("Response (1k): " + response.substring(0, Math.min(1024, response.length())));  
+                break;  
+            }  
+        }  
+  
+        catch (final IOException e) {  
+            warning(e.getMessage());  
+        }  
+  
+        finally {  
+            post.releaseConnection();  
+        }  
+  
+        return null;  
+    }  
+  
+    /** 
+     * @param server 
+     * @param username 
+     * @param password 
+     */  
+    private static String getTicketGrantingTicket(final String server, final String username, final String password) {  
+        final HttpClient client = new HttpClient();  
+  
+        final PostMethod post = new PostMethod(server);  
+  
+        post.setRequestBody(new NameValuePair[] { new NameValuePair("username", username),  
+                new NameValuePair("password", password) });  
+  
+        try {  
+            client.executeMethod(post);  
+  
+            final String response = post.getResponseBodyAsString();  
+            info("TGT="+response);  
+            switch (post.getStatusCode()) {  
+            case 201: {  
+                final Matcher matcher = Pattern.compile(".*action=\".*/(.*?)\".*").matcher(response);  
+  
+                if (matcher.matches())  
+                    return matcher.group(1);  
+  
+                warning("Successful ticket granting request, but no ticket found!");  
+                info("Response (1k): " + response.substring(0, Math.min(1024, response.length())));  
+                break;  
+            }  
+  
+            default:  
+                warning("Invalid response code (" + post.getStatusCode() + ") from CAS server!");  
+                info("Response (1k): " + response.substring(0, Math.min(1024, response.length())));  
+                break;  
+            }  
+        }  
+  
+        catch (final IOException e) {  
+            warning(e.getMessage());  
+        }  
+  
+        finally {  
+            post.releaseConnection();  
+        }  
+  
+        return null;  
+    }  
+  
+    private static void ticketValidate(String serverValidate, String serviceTicket, String service) {  
+        notNull(serviceTicket, "paramter 'serviceTicket' is not null");  
+        notNull(service, "paramter 'service' is not null");  
+  
+        final HttpClient client = new HttpClient();  
+        GetMethod post = null;  
+  
+        try {  
+            post = new GetMethod(serverValidate+"?"+"ticket="+serviceTicket+"&service="+URLEncoder.encode(service, "UTF-8"));  
+            client.executeMethod(post);  
+  
+            final String response = post.getResponseBodyAsString();  
+            info(response);  
+            switch (post.getStatusCode()) {  
+            case 200: {  
+                info("ÊàêÂäüÂèñÂæóÁî®Êà∑Êï∞ÊçÆ");  
+            }  
+            default: {  
+  
+            }  
+            }  
+  
+        } catch (Exception e) {  
+            warning(e.getMessage());  
+        } finally {  
+            //ÈáäÊîæËµÑÊ∫ê  
+            post.releaseConnection();  
+        }  
+  
+    }  
+  
+    private static void notNull(final Object object, final String message) {  
+        if (object == null)  
+            throw new IllegalArgumentException(message);  
+    }  
+  
+    public static void main(final String[] args) throws Exception {  
+        final String server = "http://l145q8w2034.iok.la:8443/cas/v1/tickets";  
+        final String username = "yxc";  
+        final String password = "123";  
+        final String service = "https://145q8w2034.iok.la:8445/webServer/login/cas";  //ÈöèÊÑèÂÜô  
+        final String proxyValidate = "http://localhost:8080/<span style='font-family: Arial, Helvetica, sans-serif;'>CASServer</span>/proxyValidate";  
+  
+          
+        ticketValidate(proxyValidate, getTicket(server, username, password, service), service);  
+          
+    }  
+  
+    private static void warning(String msg) {  
+        System.out.println(msg);  
+    }  
+  
+    private static void info(String msg) {  
+        System.out.println(msg);  
+    }  
 }
